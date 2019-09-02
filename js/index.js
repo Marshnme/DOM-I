8d5ca69e09f712bb108a263f6151edf6531f41f5
Joshua Holtsclaw <Jildsmiles@yahoo.com>
2019-09-02 17:31:33 -0400

    added all text

--- js/index.js
+++ js/index.js
@@ -52,7 +52,7 @@ middleImg.setAttribute("src", siteContent["main-content"]["middle-img-src"]
 
 
 const ctaText = document.querySelector(".cta-text");
-let newLine = "\r\n";
+const newLine = "\r\n";
 ctaText.style.whiteSpace = "pre";
 ctaText.childNodes[1].textContent = `DOM ${newLine} IS ${newLine}AWESOME`;
 ctaText.childNodes[3].textContent = "Get Started";
@@ -85,4 +85,30 @@ navBar[0].children[5].textContent = "Contact";
 // const navBar = document.querySelectorAll("nav");
 // navBar[0].children[0].textContent = "Services";
 
+
+const wordContent = document.querySelectorAll(".text-content");
+wordContent[0].children[0].textContent = "Features"
+wordContent[0].children[1].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+wordContent[1].children[0].textContent = "About"
+wordContent[1].children[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+wordContent[2].children[0].textContent = "Services";
+wordContent[2].children[1].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+wordContent[3].children[0].textContent = "Product"
+wordContent[3].children[1].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+wordContent[4].children[0].textContent = "Vision"
+wordContent[4].children[1].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+const contactSection = document.querySelectorAll(".contact");
+contactSection[0].children[1].style.whiteSpace = "pre";
+contactSection[0].children[0].textContent = "contact";
+contactSection[0].children[1].textContent = `123 Way 456 Street ${newLine} Somewhere, USA`;
+contactSection[0].children[2].textContent = "1 (888) 888-8888";
+contactSection[0].children[3].textContent = "sales@greatidea.io";
+
+const footerSelec = document.getElementsByTagName("footer");
+
+footerSelec[0].textContent = "Copyright Great Idea! 2018";
 
